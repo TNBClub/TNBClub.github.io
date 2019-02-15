32d9514cbb6edb4db131238fd2c1a939d6001d63
TNBClub <47642338+TNBClub@users.noreply.github.com>
2019-02-15 17:19:18 +0330

    First Commit

---
+++ assets/scripts/animations.js
@@ -0,0 +1,23 @@
+function addAnimation(){
+    let element =  document.querySelector('#group');
+    element.classList.add('animated', 'pulse');
+  }
+
+  function removeAnimation(){
+    let element =  document.querySelector('#group');
+    element.classList.remove('animated', 'pulse');
+  }
+
+
+
+  function addAnimation2(){
+    let element =  document.querySelector('#indivisual');
+    element.classList.add('animated', 'pulse');
+  }
+
+  function removeAnimation2(){
+    let element =  document.querySelector('#indivisual');
+    element.classList.remove('animated', 'pulse');
+  }
+
+
---
+++ assets/scripts/hamicon.js
@@ -0,0 +1,27 @@
+function hamIcon(x) {
+
+
+      x.classList.toggle("change");
+      let y = document.getElementsByClassName('main-menu-items')[0].style.display;
+
+      document.getElementsByClassName('main-menu-items')[0].style.display = 'inline-block';
+
+      
+      if(y == 'inline-block'){
+        document.getElementsByClassName('main-menu-items')[0].style.display = '';
+      }
+      else if(y == ''){
+        document.getElementsByClassName('main-menu-items')[0].style.display = 'inline-block';
+      }
+    
+      console.log(y);
+    
+    }
+
+  
+
+
+
+
+
+
